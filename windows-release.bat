f2477a8532c8f6e9f409c81b3334b9a49eb3a353
jimmyboh <jim@jimmyboh.com>
2014-04-05 18:05:44 -0400

    Removed "node.exe", but still need it.

--- windows-release.bat
+++ windows-release.bat
@@ -4,7 +4,7 @@ REM Clearing the "release" folder...
 del .\release\Overcaster.exe
 
 REM Compressing the project files...
-7z a Overcaster.zip .\node_modules .\utils .\node.exe .\server .\logo.svg .\splash.css .\splash.js .\icon.png .\loading.gif .\splash.html .\package.json
+7z a Overcaster.zip .\node_modules .\utils .\server .\logo.svg .\splash.css .\splash.js .\icon.png .\loading.gif .\splash.html .\package.json
 move Overcaster.zip .\release\Overcaster.nw
 
 REM Copy nw.exe for packaging
