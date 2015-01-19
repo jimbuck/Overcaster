839056b8d53630ee182d33212ddbd102c4f7782e
JimmyBoh <jim@jimmyboh.com>
2015-01-19 11:44:39 -0500

    Updated format and removed nested gitignore file.

--- web/app/scripts/directives/octilelayout.js
+++ web/app/scripts/directives/octilelayout.js
@@ -22,52 +22,53 @@ angular.module('overcasterDirectives')
       transclude: true,
       compile: function (element, attrs, transcludeFn) {
 
-        return function(scope, $element, $attrs) {
+        return function (scope, $element, $attrs) {
           var elements = [];
           var tiles = scope.items || [];
           var columns = scope.columns;
 
-          scope.$watch('items', function(value){
+          scope.$watch('items', function (value) {
             tiles = value || [];
             updateLayout();
           });
 
-          scope.$watch('columns', function(value){
+          scope.$watch('columns', function (value) {
             columns = value || 5;
             updateLayout();
           });
 
           updateLayout();
 
-          function updateLayout(){
+          function updateLayout() {
 
             var i, block, childScope;
 
             // check if elements have already been rendered
-            if(elements.length > 0){
+            if (elements.length > 0) {
               // if so remove them from DOM, and destroy their scope
               for (i = 0; i < elements.length; i++) {
                 elements[i].el.parent().remove();
                 elements[i].scope.$destroy();
-              };
+              }
+              ;
               elements = [];
             }
 
-            for (i = 0; i < tiles.length+($attrs.endWithNull?1:0); i++) {
+            for (i = 0; i < tiles.length + ($attrs.endWithNull ? 1 : 0); i++) {
               // create a new scope for every element in the collection.
               childScope = scope.$new();
               // pass the current element of the collection into that scope
               angular.extend(childScope, tiles[i]);
 
-              transcludeFn(childScope, function(clone){
+              transcludeFn(childScope, function (clone) {
                 // clone the transcluded element, passing in the new scope.
 
                 var el = angular.element('<div />').append(clone);
                 el.css({
                   'float': 'left',
                   'padding': 10,
-                  'width': (100/columns) + '%'
-                })
+                  'width': (100 / columns) + '%'
+                });
                 element.append(el); // add to DOM
                 block = {};
                 block.el = clone;
@@ -75,7 +76,6 @@ angular.module('overcasterDirectives')
                 elements.push(block);
               });
             };
-
           }
         };
       }
