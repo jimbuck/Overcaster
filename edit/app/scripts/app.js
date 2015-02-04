479d88e53aeb177d855d1ee47887c32c88d51c23
Eric Littlejohn <eric.littlejohn28@gmail.com>
2015-02-04 00:45:06 -0500

    Began working on Element Editor. Added View, Route, and Controller. Brought in imports for AngularUI ACE Editor (#24)

--- edit/app/scripts/app.js
+++ edit/app/scripts/app.js
@@ -17,7 +17,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'overcasterControllers'
+    'overcasterControllers',
+    'ui.ace'
   ])
   .config(function ($locationProvider, $routeProvider) {
 
@@ -47,6 +48,10 @@ angular
       .when('/sessions/:id?', {
         templateUrl: 'views/sessions.html',
         controller: 'SessionsCtrl'
+      })
+      .when('/elements/edit/:id?', {
+        templateUrl: 'views/elementeditor.html',
+        controller: 'ElementEditorCtrl'
       })
       .when('/elements/:id?', {
         templateUrl: 'views/elements.html',
