b86ac21c9102776c81fe5e80b29c650e84b08a58
Eric Littlejohn <eric.littlejohn28@gmail.com>
2015-02-04 17:29:53 -0500

    Updated the snap directives tests to remove outdate and invalid tests (#9)

--- edit/test/spec/directives/ocsnapitem.js
+++ edit/test/spec/directives/ocsnapitem.js
@@ -11,10 +11,4 @@ describe('Directive: ocSnapItem', function () {
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
   }));
-
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<oc-snap-item></oc-snap-item>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the ocSnapItem directive');
-  }));
 });
--- edit/test/spec/directives/ocsnapregion.js
+++ edit/test/spec/directives/ocsnapregion.js
@@ -11,10 +11,4 @@ describe('Directive: ocSnapRegion', function () {
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
   }));
-
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<oc-snap-region></oc-snap-region>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the ocSnapRegion directive');
-  }));
 });
