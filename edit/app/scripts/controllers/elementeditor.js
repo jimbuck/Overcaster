d36f0919573518b705e786f3247c9f69448e246e
unknown <littlejohn@littlejohnm4600.rdacorp.com>
2015-02-10 16:00:48 -0500

    Working copy of the element editor with code editing enabled

--- edit/app/scripts/controllers/elementeditor.js
+++ edit/app/scripts/controllers/elementeditor.js
@@ -13,37 +13,62 @@ angular.module('overcasterControllers')
       id: '1',
       name: 'Element Edit 1',
       path: 'path/to/element',
-      html: '',
-      css: '',
-      js: ''
+      html: '<button id="btnTest">Class Change!</button> <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>',
+      css: '.blue { background: blue; }',
+      js: '$(function() { $("#btnTest").click(function() { $("body").addClass("blue"); }); });'
     };
 
     $scope.updateElement = function() {
-      var result = htmlify($scope.currentElement.html,$scope.currentElement.css, $scope.currentElement.js);
-      console.log(result);
-      angular.element('#ElementPreviewFrame').contents().find('html').html(result);
+      var newElement = angular.element('<iframe frameborder=0></iframe>');
+      angular.element('.preview-window .resizable').html(''); //Remove existing iframe
+      angular.element('.preview-window .resizable').append(newElement); //Add the new iframe
+
+      newElement = angular.element('.preview-window .resizable').find('iframe'); //locate the newly added iframe
+
+      var rootHtml = angular.element('<html></html>')
+        .append(loadElementHead($scope.currentElement.css, $scope.currentElement.name)) //Add new head to iframe
+        .append(loadElementBody($scope.currentElement.html, $scope.currentElement.js)); //add new body to html
+
+      console.log(rootHtml[0]);
+      newElement.attr('srcdoc', rootHtml.appendTo('<div></div>').html());
     };
 
-    function htmlify(body, css, js, title){
-      var html = '<!doctype html><html><head><meta charset="utf-8"><title>' + ((title) ? title : 'Element Preview') + '</title>'; //Create Document Header
-      if (css) { //If css was provided
-        html += '<style>' + css + '</style>'; //Insert provided CSS
-      }
+    function loadElementBody(body, scripts) {
+      body = body || ''; //Handle body default
+      scripts = scripts || ''; //Handle script default
 
-      html += '</head>'; //Close Head Tag
-      html += '<body>'; //Open Body Tag
-      if (body) { //If Body Content was provided
-        html += body;
-      }
+      var bodyTag = angular.element('<body></body>');
+      var newScripts = angular.element('<script></script>'); //Create new script tag
 
-      if (js) { //If JS content was provided
-        html += '<script>' + js + '</script>'; //Insert provided javascript
-      }
+      newScripts.append(scripts); //Insert scripts
 
-      html += '</body>'; //Close Head Tag
-      html += '</html>'; //Close Html Tag
+      bodyTag
+        .append(body) //Insert body content
+        .append(newScripts); //Add scripts to the body
 
-      return html;
+      return bodyTag;
+    }
+
+    function loadElementHead(css, title) {
+      title = title || 'Element Preview'; //Handle title defaults
+      css = css || ''; //Handle css defaults
+
+      var headTag = angular.element('<head></head>');
+      var newMeta = angular.element('<meta/>'); //Create new Meta tag
+      var newTitle = angular.element('<title></title>>'); //Create new title tag
+      var newStyle = angular.element('<style type="text/css"></style>'); //Create new style tag
+
+      newMeta.attr('charset', 'UTF-8'); //Add charset attribute to meta tag
+      newTitle.text(title); //Add Title to title tag
+      newStyle.append(css); //Insert provided CSS
+
+
+      headTag
+        .append(newMeta) //Add new meta to head tag
+        .append(newTitle) //Add new title to head tag
+        .append(newStyle); //Add new style to head tag
+
+      return headTag;
     }
 
     $scope.updateElement();
